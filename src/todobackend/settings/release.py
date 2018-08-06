59700ebf88191f67c959875872462d9978422faa
Kittipoom Kumpet <kittipoom@hardwireinteractive.com>
2018-08-06 11:22:55 +0700

    add env postgres_host

--- src/todobackend/settings/release.py
+++ src/todobackend/settings/release.py
@@ -14,11 +14,11 @@ ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*')]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'db',
-        'PORT': 5432,
+        'NAME': os.environ.get('POSTGRES_DATABASE','postgres'),
+        'USER': os.environ.get('POSTGRES_USER','postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD','password'),
+        'HOST': os.environ.get('POSTGRES_HOST','db'),
+        'PORT': os.environ.get('POSTGRES_PORT','5432'),
     }
 }
 
